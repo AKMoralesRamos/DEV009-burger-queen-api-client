4326cadc5dbffa5ed1bdc1f8b436994599a6495d
AKMoralesRamos <dental.morak@gmail.com>
2023-09-09 19:08:50 -0600

    cambios-mock

--- burguer-queen-api-client/src/main.tsx
+++ burguer-queen-api-client/src/main.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+fetch('http://localhost:8080/orders')
+  .then((response) => response.json())
+  .then((data) => {
+    // Una vez que se obtengan los datos, inicializa tu aplicación
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App orders={data} /> {/* Pasa los datos como propiedades */}
+      </React.StrictMode>
+    );
+  })
+  .catch((error) => {
+    // Manejar errores de solicitud
+    console.error('Error al obtener órdenes:', error);
+  });
+  
+/* ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+ */
