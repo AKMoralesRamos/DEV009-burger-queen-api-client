9b0da16464a7a87d1fe0c88b5588d95fdd53c091
AKMoralesRamos <dental.morak@gmail.com>
2023-09-11 17:30:11 -0600

    Login-hacia-home

--- burguer-queen-api-client/src/components/login.tsx
+++ burguer-queen-api-client/src/components/login.tsx
@@ -1,21 +1,45 @@
-/* function Login() {
-    
-  return (
-    
-      <>
-        <h1>Hola, ingresa usuario y contraseña</h1>
-      </>
-    );
-  }
-
-export default Login; */
-
+import { useState } from 'react';
+//import { useHistory } from 'react-router-dom';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
-function FormFloatingBasicExample() {
+function FormLogin() {
+//  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.accessToken;
+        const concat = `Bearer ${token}`;
+        console.log(concat);
+        console.log('Inicio de sesión exitoso');
+        window.history.pushState({}, '', `${window.location.origin}/home`);
+        window.dispatchEvent(new PopStateEvent('popstate'));
+      } else {
+        // La solicitud no fue exitosa, puedes manejar los errores aquí.
+        console.error('Error al iniciar sesión');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+    }
+  };
+
   return (
     <>
       <FloatingLabel
@@ -23,16 +47,26 @@ function FormFloatingBasicExample() {
         label="Email address"
         className="mb-3"
       >
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control
+          type="email"
+          placeholder="name@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </FloatingLabel>
       <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </FloatingLabel>
-      <Button variant="success">Iniciar sesión</Button>{' '}
+      <Button variant="success" onClick={handleLogin}>
+        Iniciar sesión
+      </Button>{' '}
     </>
-    
   );
 }
 
-
-export default FormFloatingBasicExample;
+export default FormLogin;
