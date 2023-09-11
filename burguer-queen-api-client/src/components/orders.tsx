dcd78214e6810381b3ae632e814ffeda31e607a5
AKMoralesRamos <dental.morak@gmail.com>
2023-09-11 14:29:35 -0600

    inicio de rutas

---
+++ burguer-queen-api-client/src/components/orders.tsx
@@ -0,0 +1,9 @@
+function orders() {
+    return (
+      <>
+        <h1>Hola, aqui vas a visualizar órdenes de pedido</h1>
+      </>
+    );
+  }
+
+export default orders;
--- burguer-queen-api-client/src/main.tsx
+++ burguer-queen-api-client/src/main.tsx
@@ -2,27 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from './App.tsx'
+import App from './App'
 import './index.css'
-
-fetch('http://localhost:8080/orders')
-  .then((response) => response.json())
-  .then((data) => {
-    // Una vez que se obtengan los datos, inicializa tu aplicación
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-      <React.StrictMode>
-        <App orders={data} /> {/* Pasa los datos como propiedades */}
-      </React.StrictMode>
-    );
-  })
-  .catch((error) => {
-    // Manejar errores de solicitud
-    console.error('Error al obtener órdenes:', error);
-  });
   
-/* ReactDOM.createRoot(document.getElementById('root')!).render(
+
+
+/* ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+   < App />
+  </React.StrictMode>
+); */
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
- */
