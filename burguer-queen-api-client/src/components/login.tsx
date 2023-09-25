e633a8d0a19d92f68c5a35198015472a1299dc99
claucegoma <arzagitana@ciencias.unam.mx>
2023-09-25 10:03:01 -0600

    already update nueva orden y gestion

--- burguer-queen-api-client/src/components/login.tsx
+++ burguer-queen-api-client/src/components/login.tsx
@@ -3,16 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Header from './header';
 import hamburger from '../images/destacda-hamburguesa-recortada.jpg'
 import HeaderLogin from './headerLogin';
-
-
 function FormLogin() {
  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [errorMessage, setErrorMessage] = useState('');
+  const clearErrorMessage = () => {
+    setErrorMessage('');
+  };
   const handleLogin = async () => {
     try {
       const response = await fetch('http://localhost:8080/login', {
@@ -25,7 +25,6 @@ function FormLogin() {
           password,
         }),
       });
-      
       if (response.ok) {
         const data = await response.json();
         const token = data.accessToken;
@@ -36,25 +35,23 @@ function FormLogin() {
        /*  window.history.pushState({}, '', `${window.location.origin}/home`);
         window.dispatchEvent(new PopStateEvent('popstate')); */
         navigate("/home");
-      } else {
-        
-        console.error('Error al iniciar sesión');
+      } else if(!response.ok){
+        setErrorMessage('Credenciales inválidas');
       }
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
     }
   };
-
   return (
     <>
    <HeaderLogin />
     <div
     style={{
-      backgroundImage: `url(${hamburger})`, 
-      backgroundSize: 'cover', 
-      backgroundRepeat: 'no-repeat', 
-      backgroundPosition: 'center', 
-      minHeight: '100vh', 
+      backgroundImage: `url(${hamburger})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '100vh',
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'flex-start',
@@ -63,7 +60,7 @@ function FormLogin() {
   >
     <div
       style={{
-        backgroundColor: '#EC8133', 
+        backgroundColor: '#EC8133',
         padding: '50px',
         borderRadius: '5px',
         boxShadow: '9px 10px 13px -1px rgba(0,0,0,0.4)',
@@ -81,17 +78,20 @@ function FormLogin() {
           placeholder="name@example.com"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          onClick={clearErrorMessage}
         />
       </FloatingLabel>
       <FloatingLabel controlId="floatingPassword" label="Contraseña"
-        className="mb-5">
+        className="mb-4">
         <Form.Control
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onClick={clearErrorMessage}
         />
       </FloatingLabel>
+      <div className="mb-4" style={{ color: 'black' }}>{errorMessage}</div>
       <Button variant="dark" onClick={handleLogin} style={{  width: '90%',
     height: '10%',borderRadius: '50px' }}>
         Iniciar sesión
@@ -101,5 +101,4 @@ function FormLogin() {
   </>
   );
 }
-
 export default FormLogin;
